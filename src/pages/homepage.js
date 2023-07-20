23e8f8aad9621d0d193b6f4c90c4de0d3e990a14
PC <YaredAlex>
2023-07-20 15:54:35 +0530

    adding home page

---
+++ src/pages/homepage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./home.css";
+const Homepage = () => {
+  return (
+    <div className="main-bar">
+      <div>
+        <h1>Analysis</h1>
+        <hr />
+      </div>
+    </div>
+  );
+};
+
+export default Homepage;
