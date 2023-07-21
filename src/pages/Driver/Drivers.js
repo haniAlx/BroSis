3d140633e5028729902540f689f2deb5f6a2a2f9
PC <YaredAlex>
2023-07-21 18:27:21 +0530

    creating Driver page

---
+++ src/pages/Driver/Drivers.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Drivers() {
+  return (
+    <div className="main-bar">
+      <div>
+        <h2 style={{}}>Driver</h2>
+        <hr className="hr" />
+        <div className="chart-container"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Drivers;
