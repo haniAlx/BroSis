dab78336348bfedc67dc8faf2ff479c744e38c7a
PC <YaredAlex>
2023-07-21 15:21:14 +0530

    routes configured and protecting route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
-import {BrowserRouter, Routes,Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navigation from "./components/Navigation";
 import SideBar from "./components/SideBar";
 import Homepage from "./pages/homepage";
@@ -9,26 +9,36 @@ import ForgatePass from "./components/SignInPages/ForgatePass";
 import ConfirmPass from "./components/SignInPages/ConfirmPass";
 
 function App() {
+  const [user, setUser] = useState();
+  useEffect(() => {
+    const getUser = () => {
+      setUser(localStorage.getItem("user"));
+    };
+    getUser();
+  }, []);
   return (
     <>
       <BrowserRouter>
-            <Routes>
-                  <Route path='/' element={<SignIn />}></Route>  
-                  <Route path='/ForgatePass' element={<ForgatePass />}></Route>  
-                  <Route path='/ConfirmPass' element={<ConfirmPass />}></Route> 
-                  <Route path='/Dashboard' element={
-                    <div>
-                    <Navigation />
-                    <div className="main-container">
-                      <div className="container_mv">
-                        <SideBar />
-                        <Homepage />
-                      </div>
-                    </div>
-                    </div>
-                  }></Route> 
-            </Routes>
-          </BrowserRouter>
+        {user || null ? (
+          <div>
+            <Navigation />
+            <div className="main-container">
+              <div className="container_mv">
+                <SideBar />
+                <Routes>
+                  <Route path="/Dashboard" element={<Homepage />}></Route>
+                </Routes>
+              </div>
+            </div>
+          </div>
+        ) : (
+          <Routes>
+            <Route path="/" element={<SignIn />}></Route>
+            <Route path="/ForgatePass" element={<ForgatePass />}></Route>
+            <Route path="/ConfirmPass" element={<ConfirmPass />}></Route>
+          </Routes>
+        )}
+      </BrowserRouter>
     </>
   );
 }
