ae09abddb6aefcc37db01efdc3c0a93454da8a94
PC <YaredAlex>
2023-07-22 01:01:31 +0530

    creating drivertable

---
+++ src/pages/Driver/DriversTable.js
@@ -0,0 +1,55 @@
+import React from "react";
+
+const DriversTable = ({ target }) => {
+  return (
+    <div>
+      <table className="driver-table">
+        <thead>
+          <tr>
+            <th>Driver Name</th>
+            <th>License Number</th>
+            <th>Experience</th>
+            <th>LicenseGrade</th>
+            <th>Status</th>
+            <th>VehicleOwner</th>
+            <th>Detail</th>
+            <th>Tracking</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {target.map((item, index) => (
+            <tr className="" key={index}>
+              <td>{item.driverName}</td>
+              <td>{item.licenseNumber}</td>
+              <td>{item.experience}</td>
+              <td>{item.licenseGrade}</td>
+              <td
+                style={{
+                  color: `${
+                    item.status.toLowerCase() === "unassigned"
+                      ? "red"
+                      : item.status.toLowerCase() === "assigned"
+                      ? "green"
+                      : "orange"
+                  }`,
+                }}
+              >
+                {item.status}
+              </td>
+              <td>{item.vehicleOwner}</td>
+              <td>
+                <button className="table-btn">Detail</button>
+              </td>
+              <td>
+                <button className="table-btn">Manage</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default DriversTable;
