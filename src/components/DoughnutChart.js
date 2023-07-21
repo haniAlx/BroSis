5bf16e7b3ed796be9c5aeaed626b6d3317a7a003
PC <YaredAlex>
2023-07-21 17:47:01 +0530

    adding DoughnutChart.js

---
+++ src/components/DoughnutChart.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Doughnut } from "react-chartjs-2";
+
+const DoughnutChart = ({ chartData }) => {
+  return (
+    <>
+      <Doughnut data={chartData} />
+    </>
+  );
+};
+
+export default DoughnutChart;
