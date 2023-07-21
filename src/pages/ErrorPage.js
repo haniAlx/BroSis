9dfab3450d48c14c6b59af0d0845f65fbe0e4890
PC <YaredAlex>
2023-07-21 16:17:55 +0530

    adding illustration

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "./errorPage.css";
+import imgNotfound from "../assets/image/cuate.png";
 function ErrorPage() {
   return (
     <div className="error-container">
       <div className="error-msg">
+        <img src={imgNotfound} />
         <h1>404</h1>
         <h2>Page Not found</h2>
       </div>
