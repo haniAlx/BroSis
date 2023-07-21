f9fdae77280d3c35d426a168a7871fb1fe8e7088
PC <YaredAlex>
2023-07-21 15:22:21 +0530

    adding function to navigation

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   MdDashboard,
   MdDirectionsCar,
@@ -6,26 +6,56 @@ import {
   MdPeople,
 } from "react-icons/md";
 import "./sidebar.css";
+import { useNavigate } from "react-router-dom";
 const SideBar = () => {
+  const [current, setCurrent] = useState({
+    dashboard: true,
+    vehicle: false,
+    driver: false,
+    users: false,
+  });
+  const navigator = useNavigate();
+  const changeRoute = (val) => {
+    setCurrent({
+      dashboard: false,
+      vehicle: false,
+      driver: false,
+      users: false,
+      [val]: true,
+    });
+    navigator(`/${val}`);
+  };
   return (
     <div>
       <div className="sidebar-container">
         <div className="sidebar-items">
           <ul className="sidebar-list">
-            <li>
-              <MdDashboard color="yellow" size={25} />
+            <li onClick={() => changeRoute("dashboard")}>
+              <MdDashboard
+                color={`${current.dashboard ? "Yellow" : "black"}`}
+                size={25}
+              />
               <p>Home</p>
             </li>
-            <li>
-              <MdDirectionsCar color="black" size={25} />
+            <li onClick={() => changeRoute("vehicle")}>
+              <MdDirectionsCar
+                color={`${current.vehicle ? "Yellow" : "black"}`}
+                size={25}
+              />
               <p>Vehicle</p>
             </li>
-            <li>
-              <MdOutlineManageAccounts color="black" size={25} />
+            <li onClick={() => changeRoute("driver")}>
+              <MdOutlineManageAccounts
+                color={`${current.driver ? "Yellow" : "black"}`}
+                size={25}
+              />
               <p>Driver</p>
             </li>
-            <li>
-              <MdPeople color="black" size={25} />
+            <li onClick={() => changeRoute("users")}>
+              <MdPeople
+                color={`${current.users ? "Yellow" : "black"}`}
+                size={25}
+              />
               <p>Users</p>
             </li>
           </ul>
