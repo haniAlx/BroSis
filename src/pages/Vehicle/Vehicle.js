2e61d6f85f1d56d1beb0f8b1d8c9a3740d1828fa
PC <YaredAlex>
2023-07-21 18:27:48 +0530

    making VehiclePage

---
+++ src/pages/Vehicle/Vehicle.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Vehicle = () => {
+  return (
+    <div className="main-bar">
+      <div>
+        <h2 style={{}}>Vehicles</h2>
+        <hr className="hr" />
+        <div className="chart-container"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Vehicle;
