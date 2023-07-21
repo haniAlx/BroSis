f2124774561e951886742f8f20913ae0030bf951
PC <YaredAlex>
2023-07-21 15:53:57 +0530

    Creating Error page

---
+++ src/pages/ErrorPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./errorPage.css";
+function ErrorPage() {
+  return (
+    <div className="error-container">
+      <div className="error-msg">
+        <h1>404</h1>
+        <h2>Page Not found</h2>
+      </div>
+    </div>
+  );
+}
+
+export default ErrorPage;
