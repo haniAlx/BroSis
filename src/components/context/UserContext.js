17ec033a744d20151bd5cfcf23601115f0866143
PC <YaredAlex>
2023-07-23 02:16:19 +0530

    making user context to control user globaly

---
+++ src/components/context/UserContext.js
@@ -0,0 +1,19 @@
+import React, { createContext, useContext, useEffect, useState } from "react";
+
+const ContextUser = createContext(null);
+export function useUserContext() {
+  return useContext(ContextUser);
+}
+function UserContext({ children }) {
+  const [currentUser, setCurrentUser] = useState("");
+  useEffect(() => {
+    setCurrentUser(JSON.parse(localStorage.getItem("user")));
+  }, []);
+  return (
+    <ContextUser.Provider value={{ currentUser, setCurrentUser }}>
+      {children}
+    </ContextUser.Provider>
+  );
+}
+
+export default UserContext;
