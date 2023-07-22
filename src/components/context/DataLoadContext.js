7c1b4ff04ef41a53cdb068a37f63704b609c306c
PC <YaredAlex>
2023-07-22 12:38:28 +0530

    making variable global

--- src/components/context/DataLoadContext.js
+++ src/components/context/DataLoadContext.js
@@ -1,11 +1,134 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const LoadDataContext = React.createContext(null);
 export const useLoadContext = () => useContext(LoadDataContext);
 const DataLoadContext = ({ children }) => {
   const [load, setLoad] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [allDrivers, setAllDrivers] = useState([]);
+  const [onRoute, setOnRoute] = useState([]);
+  const [Assigned, setAssigned] = useState([]);
+  const [unassigned, setUnassigned] = useState([]);
+  const [permit, setPermit] = useState([]);
+  const [error, setError] = useState();
+  const [tableData, setTableData] = useState(allDrivers);
+  const [allDataLoaded, setAllDataLoaded] = useState(false);
+  const apiAllDrivers = "http://64.226.104.50:9090/Api/Admin/All/Drivers";
+  const apiOnRoute = "http://64.226.104.50:9090/Api/Admin/Drivers/ONROUTE";
+  const apiAssigned = "http://64.226.104.50:9090/Api/Admin/Drivers/ASSIGNED";
+  const apiUnAssigned =
+    "http://64.226.104.50:9090/Api/Admin/Drivers/UNASSIGNED";
+  const apiPermit = "http://64.226.104.50:9090/Api/Admin/Drivers/PERMIT";
+  const navigator = useNavigate();
+  /**  GET JWT */
+  const jwt = JSON.parse(localStorage.getItem("jwt"));
+  /** Option for fetch */
+  const options = {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${jwt}`,
+      Accept: "application/json",
+    },
+  };
+  /** Aborting fetch if it is taking to long */
+  //   const controler = new AbortController();
+  //   const timeout = setTimeout(() => {
+  //     controler.abort();
+  //     console.log("fetch aborted");
+  //     setLoading(false);
+  //     if (!allDataLoaded) setError("Unable to Connet to Database");
+  //   }, 6000);
+  useEffect(() => {
+    const getAllApiData = async () => {
+      await getAllDrivers();
+      await getAssigned();
+      await getOnRoute();
+      await getUnAssigned();
+      await getPermit();
+      setLoading(false);
+    };
+    setLoading(true);
+    getAllApiData();
+
+    return () => {};
+  }, []);
+  const getAllDrivers = async () => {
+    try {
+      const res = await fetch(apiAllDrivers, options);
+      console.log("response", res.status);
+      if (res.status === 401) console.log("session expired");
+      localStorage.clear();
+      navigator("/");
+      const data = await res.json();
+      if (data.drivers) {
+        setAllDrivers(data.drivers);
+        setTableData(data.drivers);
+      }
+    } catch (e) {
+      console.log(e.message);
+      setError(e.message);
+      setLoading(false);
+    }
+  };
+  const getOnRoute = async () => {
+    try {
+      const res = await fetch(apiOnRoute, options);
+      const data = await res.json();
+      if (data.drivers) setOnRoute(data.drivers);
+      console.log(data.drivers);
+    } catch (e) {
+      console.log(e.message);
+      setError(e.message);
+    }
+  };
+  const getAssigned = async () => {
+    try {
+      const res = await fetch(apiAssigned, options);
+      const data = await res.json();
+      if (data.drivers) setAssigned(data.drivers);
+      console.log(data.drivers);
+    } catch (e) {
+      console.log(e.message);
+      setError(e.message);
+    }
+  };
+  const getUnAssigned = async () => {
+    try {
+      const res = await fetch(apiUnAssigned, options);
+      const data = await res.json();
+      if (data.drivers) setUnassigned(data.drivers);
+      console.log(data.drivers);
+    } catch (e) {
+      console.log(e.message);
+      setError(e.message);
+    }
+  };
+  const getPermit = async () => {
+    try {
+      const res = await fetch(apiPermit, options);
+      const data = await res.json();
+      if (data.drivers) setPermit(data.drivers);
+      console.log(data.drivers);
+    } catch (e) {
+      console.log(e.message);
+      setError(e.message);
+    }
+  };
   return (
-    <LoadDataContext.Provider value={{ load, setLoad: setLoad }}>
+    <LoadDataContext.Provider
+      value={{
+        loading,
+        setLoad: setLoad,
+        payload: {
+          allDrivers: allDrivers,
+          onRoute: onRoute,
+          assigned: Assigned,
+          unassigned: unassigned,
+          permit: permit,
+        },
+      }}
+    >
       {children}
     </LoadDataContext.Provider>
   );
