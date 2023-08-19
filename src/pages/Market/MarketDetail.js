3821f7f697db73d96fba51d4cbb37a096e04e1ff
PC <YaredAlex>
2023-08-19 13:32:52 +0530

    Active and finished function added

--- src/pages/Market/MarketDetail.js
+++ src/pages/Market/MarketDetail.js
@@ -1,12 +1,17 @@
 import React, { useEffect, useState } from "react";
 import ReactLoading from "react-loading";
-import DoughnutChart from "../../components/Charts/DoughnutChart";
+import "./market_input.css";
 import CircularBar from "../../components/circularBar/circularBar";
 import "./market_chart.css";
 import { mainAPI } from "../../components/mainAPI";
-import { useParams } from "react-router-dom";
-import { showErrorMessage } from "../../components/SwalMessages";
+import { useNavigate, useParams } from "react-router-dom";
+import {
+  showErrorMessage,
+  showSuccessMessage,
+} from "../../components/SwalMessages";
 import MarketDetailTable from "./MarketDetailTable";
+import LoadingPage from "../../components/LoadingPage";
+import DoughnutChart from "../../components/Charts/DoughnutChart";
 const MarketDetail = () => {
   const { id } = useParams();
   const [loading, setLoading] = useState(false);
@@ -16,9 +21,10 @@ const MarketDetail = () => {
   const [remaining, setRemaining] = useState("");
   const [weight, setWeight] = useState("");
   const [cargoData, setCargoData] = useState({});
+  const [reload, setReload] = useState(false);
   const [chartData, setChartData] = useState({
     labels: ["FINISHED", "REMAINING"],
-    datasets: [{ label: "Summary", data: [0, 0] }],
+    datasets: [{ label: "Summary", data: [5, 2] }],
   });
   const apiCargoDriver = `${mainAPI}/Api/Admin/All/CargoDrivers/${id}`;
   const apiCargo = `${mainAPI}/Api/Admin/All/Cargos/${id}`;
@@ -39,22 +45,26 @@ const MarketDetail = () => {
         setCargoDriver(res.cargoDriversINFs);
       })
       .catch((e) => {
-        showErrorMessage({ message: "error" });
-        console.log(e);
+        if (e.message == "Failed to fetch")
+          showErrorMessage({ message: "net::ERR_INTERNET_DISCONNECTED" });
+        else showErrorMessage({ message: "some thing went wrong" });
+        console.log(e.message);
       })
       .finally(setLoading(false));
   };
   useEffect(() => {
-    setChartData({
-      labels: ["FINISHED", "REMAINING"],
-      datasets: [
-        {
-          label: "Summary",
-          data: [8, 2],
-          backgroundColor: ["rgb(54, 162, 235)", "rgb(255, 99, 132)"],
-        },
-      ],
-    });
+    // setChartData(
+    //   {
+    //     labels: ["FINISHED", "REMAINING"],
+    //     datasets: [
+    //       {
+    //         label: "Summary",
+    //         data: [8, 2],
+    //         backgroundColor: ["rgb(54, 162, 235)", "rgb(255, 99, 132)"],
+    //       },
+    //     ],
+    //   }[reload]
+    // );
     const getCargoDetail = async () => {
       setLoading(true);
       fetch(apiCargo, options)
@@ -77,7 +87,7 @@ const MarketDetail = () => {
   return (
     <div className="main-bar">
       <div>
-        <h2 style={{}}>Market Detail</h2>
+        <h2 style={{}}>Market Detail / {cargoData.cargoOwner} </h2>
         <hr className="hr" />
         <div className="main-bar-content">
           {loading && (
@@ -118,82 +128,22 @@ const MarketDetail = () => {
           )}
           {!loading && !error && (
             <>
-              <div className="chart-container char-market">
-                <CircularBar
-                  text={loading ? "loading" : "Finished"}
-                  max={((weight - remaining) / weight) * 100}
-                  // (12/20)*100
-                  color={""}
+              {cargoData.status == "NEW" ? (
+                <NewMarket
+                  cargoData={cargoData}
+                  reload={reload}
+                  setReload={setReload}
                 />
-                <div>
-                  <p>
-                    Total weight{" "}
-                    <span style={{ fontWeight: "bold" }}> {weight}</span>
-                  </p>
-
-                  <p>
-                    Remaining{" "}
-                    <span style={{ fontWeight: "bold" }}> {remaining}</span>
-                  </p>
-                </div>
-                <CircularBar
-                  text={loading ? "loading" : "Remaining"}
-                  max={(remaining / weight) * 100}
-                  color={""}
+              ) : (
+                <ActiveMarket
+                  cargoData={cargoData}
+                  loading={loading}
+                  weight={weight}
+                  remaining={remaining}
+                  cargoDriver={cargoDriver}
+                  chartData={chartData}
                 />
-
-                {/* <div style={{ maxWidth: "300px", position: "relative" }}>
-                  <DoughnutChart chartData={chartData} />
-                  <div style={{ position: "absolute", top: "20px" }}>
-                    <p>75</p>
-                    <p>20</p>
-                  </div>
-                </div> */}
-              </div>
-              <div
-                style={{
-                  display: "flex",
-                  justifyContent: "space-around",
-                  flexWrap: "wrap",
-                  maxWidth: "800px",
-                  rowGap: "25px",
-                  marginTop: "30px",
-                  marginLeft: "auto",
-                  marginRight: "auto",
-                }}
-              >
-                <div>
-                  <div className="cargo-d">
-                    <p>Cargo Owner</p>
-                    <p className="underline"> {cargoData.cargoOwner}</p>
-                  </div>
-                  <div className="cargo-d">
-                    <p>Packaging </p>
-                    <p className="underline">{cargoData.packaging}</p>
-                  </div>
-                </div>
-                <div>
-                  <div className="cargo-d">
-                    <p>Payment</p>
-                    <p className="underline"> {cargoData.payment || "NULL"}</p>
-                  </div>
-                  <div className="cargo-d">
-                    <p>Cargo Type</p>
-                    <p className="underline"> {cargoData.cargoType}</p>
-                  </div>
-                </div>
-                <div>
-                  <div className="cargo-d">
-                    <p>Pick Up</p>
-                    <p className="underline">{cargoData.pickUp}</p>
-                  </div>
-                  <div className="cargo-d">
-                    <p>Drop Off</p>
-                    <p className="underline">{cargoData.dropOff}</p>
-                  </div>
-                </div>
-              </div>
-              <MarketDetailTable target={cargoDriver} />
+              )}
             </>
           )}
         </div>
@@ -201,5 +151,217 @@ const MarketDetail = () => {
     </div>
   );
 };
+//Active Market
+const ActiveMarket = ({
+  cargoData,
+  loading,
+  weight,
+  remaining,
+  cargoDriver,
+  chartData,
+}) => {
+  const id = cargoData.id;
+  console.log(id);
+  const jwt = JSON.parse(localStorage.getItem("jwt"));
 
+  ///    HANDLE SENDTOCARGO FUNCTION
+  const handleSendToCargo = async () => {
+    //showSuccessMessage({ message: "Cargo handled" });
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        Authorization: `Bearer ${jwt}`,
+      },
+    };
+    const sendToCargoApi = `${mainAPI}/Api/Admin/AcceptedCargo/${id}`;
+
+    try {
+      const res = await fetch(sendToCargoApi, options);
+      if (res.ok) {
+        const data = await res.json();
+        console.log(data);
+        showSuccessMessage({ message: "success" });
+      }
+    } catch (e) {
+      showErrorMessage({ message: e.message });
+    }
+  };
+  //
+  return (
+    <>
+      <div className="chart-container char-market">
+        <CircularBar
+          text={loading ? "loading" : "Finished"}
+          max={((weight - remaining) / weight) * 100}
+          color={"green"}
+          bgcolor={"#88F6A0"}
+        />
+        <div>
+          <p>
+            Total weight <span style={{ fontWeight: "bold" }}> {weight}</span>
+          </p>
+
+          <p>
+            Remaining <span style={{ fontWeight: "bold" }}> {remaining}</span>
+          </p>
+        </div>
+        <CircularBar
+          text={loading ? "loading" : "Remaining"}
+          max={(remaining / weight) * 100}
+          color={"red"}
+          bgcolor={"#FF8378"}
+        />
+        {/* 
+        <div style={{ maxWidth: "300px", position: "relative" }}>
+          <DoughnutChart chartData={chartData} />
+          <div style={{ position: "absolute", top: "20px" }}>
+            <p>75</p>
+            <p>20</p>
+          </div>
+        </div> */}
+      </div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-around",
+          flexWrap: "wrap",
+          maxWidth: "800px",
+          rowGap: "25px",
+          marginTop: "30px",
+          marginLeft: "auto",
+          marginRight: "auto",
+        }}
+      >
+        <div>
+          <div className="cargo-d">
+            <p>Date</p>
+            <p className="underline"> {cargoData.date}</p>
+          </div>
+          <div className="cargo-d">
+            <p>Packaging </p>
+            <p className="underline">{cargoData.packaging}</p>
+          </div>
+        </div>
+        <div>
+          <div className="cargo-d">
+            <p>Payment</p>
+            <p className="underline"> {cargoData.payment || "NULL"}</p>
+          </div>
+          <div className="cargo-d">
+            <p>Cargo Type</p>
+            <p className="underline"> {cargoData.cargoType}</p>
+          </div>
+        </div>
+        <div>
+          <div className="cargo-d">
+            <p style={{ textAlign: "center" }}>Trip</p>
+            <p className="underline">
+              {cargoData.pickUp} <span style={{ fontWeight: "bold" }}>To</span>{" "}
+              {cargoData.dropOff}
+            </p>
+          </div>
+          <div className="cargo-d">
+            <p>Status</p>
+            <p className="underline">{cargoData.status}</p>
+          </div>
+        </div>
+      </div>
+      {cargoData.status == "ACCEPT" ? (
+        <button
+          onClick={() => handleSendToCargo()}
+          className="btn w-300 mx-auto btn-bg-blue"
+        >
+          Send To Cargo Owner
+        </button>
+      ) : (
+        ""
+      )}
+
+      <MarketDetailTable target={cargoDriver} marketStatus={cargoData.status} />
+    </>
+  );
+};
+//New Market data
+const NewMarket = ({ cargoData, reload, setReload }) => {
+  const [price, setPrice] = useState();
+  const [loading, setLoading] = useState(false);
+  const handlePost = () => {
+    if (!price) {
+      showErrorMessage({ message: "Please set Price" });
+    } else postJob();
+  };
+  const postJob = async () => {
+    const jwt = JSON.parse(localStorage.getItem("jwt"));
+    const item = { price: price };
+    const id = cargoData.id;
+    console.log("user id is", id);
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        Authorization: `Bearer ${jwt}`,
+      },
+      body: JSON.stringify(item),
+    };
+    const postApi = `${mainAPI}/Api/Admin/PostCargo/${id}`;
+    setLoading(true);
+    try {
+      const res = await fetch(postApi, options);
+      if (res.ok) {
+        const data = await res.json();
+        showSuccessMessage({ message: data.message });
+      }
+    } catch (e) {
+      showErrorMessage({ message: e });
+    } finally {
+      setLoading(false);
+      setReload(reload); // Reloading the Page for getting status;
+    }
+  };
+  return (
+    <div>
+      {loading ? <LoadingPage message={"Posting Cargo"} /> : ""}
+      <div className="manage-window input new-market-inputs">
+        <div>
+          <label>Cargo Owner</label>
+          <input type="text" disabled defaultValue={cargoData.cargoOwner} />
+        </div>
+        <div>
+          <label>Packaging</label>
+          <input type="text" disabled defaultValue={cargoData.packaging} />
+        </div>
+        <div>
+          <label>Type</label>
+          <input type="text" disabled defaultValue={cargoData.cargoType} />
+        </div>
+        <div>
+          <label>Pickup</label>
+          <input type="text" disabled defaultValue={cargoData.pickUp} />
+        </div>
+        <div>
+          <label>Drop Off</label>
+          <input type="text" disabled defaultValue={cargoData.dropOff} />
+        </div>
+        <div>
+          <label>Weight</label>
+          <input type="text" disabled defaultValue={cargoData.weight} />
+        </div>
+        <div>
+          <label>Price</label>
+          <input
+            type="text"
+            required
+            onChange={(e) => setPrice(e.target.value)}
+          />
+        </div>
+      </div>
+      <button className="btn w-300 mx-auto" onClick={() => handlePost()}>
+        Post
+      </button>
+    </div>
+  );
+};
 export default MarketDetail;
