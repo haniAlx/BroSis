a3e5def8ac49fb8f065c6d02dc19ba7dfe426fbc
Hana447 <alemayehuhana22@gmail.com>
2023-08-24 15:08:26 +0300

    update the forrgate pass  page

--- src/components/SignInPages/ForgatePass.jsx
+++ src/components/SignInPages/ForgatePass.jsx
@@ -1,8 +1,63 @@
-import React from 'react'
+import React,{useState} from 'react'
 import  './Signin.css'
+import swal from "sweetalert";
 import { Link } from 'react-router-dom'
 export default function ForgatePass(){
 
+
+    let [isConfirm,setisConfirm] = useState(false)
+    const [phoneNumber, setPhoneNumber] = useState('');
+    let mess
+    function onSubmit(e)
+    {
+        e.preventDefault()
+        handleForgate()
+    }
+    async function handleForgate() {
+        let item =
+        {
+            phoneNumber,
+        };
+        console.log(item)
+        const options = {
+            method: "POST",
+            headers: { 'Content-Type': 'application/json', 
+            "Accept": "application/json"},
+            body: JSON.stringify(item),
+        };
+        const url = "http://164.90.174.113:9090/Api/User/GeneratePIN";
+        try {
+            const response = await fetch(url, options);
+            const result = await response.json();
+            localStorage.setItem("message", JSON.stringify(result["message"]));
+             mess = localStorage.getItem("message");
+            if (response.ok) {
+                console.log(mess)
+                setisConfirm(!isConfirm)
+                swal("Get your Pin beffore it's to late ", `${mess}`, "success", { buttons: false, timer: 5000, });
+                
+            } else {
+                console.log("failed");
+                swal(`Failed To Register ${mess}`, "error");
+            }
+        } catch (error) {
+            console.error(error);
+            swal({
+                title: "something went wrong",
+                text: `net::ERR_INTERNET_DISCONNECTED`,
+                icon: "warning",
+                dangerMode: true,
+                buttons: {
+                  confirm: true,
+                },
+                cancelButtonColor: "#d33",
+                showClass: {
+                  popup: "animate__animated animate__shakeX",
+                },
+              });
+
+        }
+    }
 
     return(
         <>
@@ -17,15 +72,21 @@ export default function ForgatePass(){
 
                     </div>
                 </div>
-                    <form  className='SigninForm'>
+                    <form  className='SigninForm' onSubmit={onSubmit}>
                         <h1>Generate Pin</h1>
                         <div className='Signin'>
                             <label>Phone number</label>
-                            <input type='text' placeholder='Enter your phone number'></input>
+                            <input type='phone'
+                             placeholder='Enter your phone number'
+                             value = {phoneNumber} onChange={e => {
+                                setPhoneNumber(e.target.value)
+                                }}
+                                name="phoneNumber"
+                            ></input>
                            <Link to='/ConfirmPass'> <p type='button'>Confirm Password</p> </Link>
 
                         </div>
-                        <button className='signIn-btn'>Sign in</button>
+                        <button className='signIn-btn'>Generate</button>
                     </form> 
                 
                 
