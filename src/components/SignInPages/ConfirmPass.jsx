868b646154a6f3fa645c197d848c94b7adfe2f22
Hana447 <alemayehuhana22@gmail.com>
2023-08-24 15:07:55 +0300

    update the confirmation page

--- src/components/SignInPages/ConfirmPass.jsx
+++ src/components/SignInPages/ConfirmPass.jsx
@@ -1,9 +1,100 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './Signin.css'
+import swal from "sweetalert";
 import { Link } from 'react-router-dom'
+import { AiOutlineEye } from "react-icons/ai";
+
 export default function ConfirmPass(){
 
+    const [error, setError] = useState(false);
+    const [pass, showPass] = useState(false);
+    const [confirm, setConfirm]= useState(false)
 
+    let [isConfirmed, setisConfirmed] =useState(false)
+    let mess
+     let isPin =false
+     if(isPin){
+        mess = localStorage.getItem("message")
+        console.log(mess)
+
+     }
+     const toggleEye = (name) => {
+     if(name === 'confirm') setConfirm(!confirm)
+     else   showPass(!pass);
+      };
+    const [username, setusername] = useState("");
+    const [pin, setpin]= useState("")
+    const [newpassword, setnewpassword] =useState("")
+    const [confirmPassword, setconfirmpassword]= useState("")
+    
+
+    function onSubmit(e)
+    {
+        e.preventDefault()
+        handleConfirm()
+    }
+    
+   /******************************Confirm *********/
+
+   async function handleConfirm() {
+
+   let item = { 
+               newpassword,
+              confirmPassword,
+              username,
+              pin
+            }
+                console.log(item)
+    const options = {
+        method: "POST",
+        headers: { "Content-Type": "application/json", Accept: "application/json", Authorization: `Bearer`, },
+        body: JSON.stringify(item),
+    };
+    const url = "http://164.90.174.113:9090/Api/User/SetPin";
+    try {
+        const response = await fetch(url, options);
+        const result = await response.json();
+        localStorage.setItem("message", JSON.stringify(result["message"]));
+      const  mess = localStorage.getItem("message");
+      console.log(mess)
+        if (response.ok) {
+            console.log(mess)
+            swal("Successful",`${mess}`, "success", { buttons: false, timer: 2000, })
+              setTimeout(()=>{setisConfirmed(!isConfirmed)}, 1000)
+        } else {
+             swal({
+                title: "Failed To Change",
+                text: `${mess} `,
+                icon: "error",
+                dangerMode: true,
+                buttons: {
+                  confirm: true,
+                },
+                cancelButtonColor: "#d33",
+                showClass: {
+                  popup: "animate__animated animate__shakeX",
+                },
+              });
+            
+        }
+    } catch (error) {
+        console.log(error + "error");
+        swal({
+            title: "something went wrong",
+            text: `net::ERR_INTERNET_DISCONNECTED`,
+            icon: "warning",
+            dangerMode: true,
+            buttons: {
+              confirm: true,
+            },
+            cancelButtonColor: "#d33",
+            showClass: {
+              popup: "animate__animated animate__shakeX",
+            },
+          });
+        // window.location.href = "/dashboard"; 
+    }
+}
     return(
         <>
             <div className='SigninWrapper'>
@@ -17,21 +108,57 @@ export default function ConfirmPass(){
 
                     </div>
                 </div>
-                    <form  className='SigninForm'>
-                        <h1>Generate Pin</h1>
+                    <form  className='SigninForm' onSubmit={onSubmit}>
+                        <h1>Change Password</h1>
                         <div className='Signin  confirm'>
                             <label>Phone number</label>
-                            <input type='text' placeholder='Enter your phone number'></input>
+                            <input type='text' 
+                            placeholder='Enter your phone number'
+                            onChange={e => setusername(e.target.value)} 
+                            name="username"
+                            ></input>
                             <label>PIN</label>
-                            <input type='password' placeholder='Enter your Pin'/>
+                            <input type='password'
+                             placeholder='Enter your Pin'
+                             onChange={e => setpin(e.target.value)}
+                                   name="pin"
+                                   />
                             <label>Password</label>
-                            <input type='password' placeholder='Enter your password'/>
+                            <div className="passWrapper">
+                                <input
+                                    id="passInput"
+                                    type={pass ? "text" : "password"}
+                                    placeholder="Password"
+                                    onChange={e => setnewpassword(e.target.value)} 
+                                    name="newpassword"
+                                />
+                                <AiOutlineEye onClick={()=>toggleEye('pass')} className="eye" />
+                                </div>
+                                {error && newpassword.length <= 0 ? (
+                                <span className="validateText">please enter your password</span>
+                                ) : (
+                                ""
+                                )}
                             <label>Confirm Password</label>
-                            <input type='password' placeholder='Confirm password'/>
+                            <div className="passWrapper">
+                                <input
+                                    id="passInput"
+                                    type={confirm ? "text" : "password"}
+                                    placeholder="Password"
+                                    onChange={e => setconfirmpassword(e.target.value)} 
+                                    name='confirmpassword'
+                                />
+                                <AiOutlineEye onClick={()=>toggleEye('confirm')} className="eye" />
+                                </div>
+                                {newpassword !== confirmPassword ? (
+                                <span className="validateText">password did not match</span>
+                                ) : (
+                                ""
+                                )}
                            <Link to='/'> <p type='button'>Sign in</p> </Link>
 
                         </div>
-                        <button className='signIn-btn'>Sign in</button>
+                        <button className='signIn-btn'>Change Password</button>
                     </form> 
                 
                 
