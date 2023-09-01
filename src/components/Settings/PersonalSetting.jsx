cf3aab54b40e16147c6a3014e27365c21fcc9593
Hana447 <alemayehuhana22@gmail.com>
2023-09-02 01:34:38 +0300

    add personal setting component

---
+++ src/components/Settings/PersonalSetting.jsx
@@ -0,0 +1,34 @@
+import React, { useState,useEffect } from "react";
+import { MdKeyboardArrowDown } from "react-icons/md";
+import { useSearchParams } from "react-router-dom";
+import { mainAPI } from "../../components/mainAPI";
+import { showErrorMessage } from "../../components/SwalMessages";
+import SettingList from './SettingList';
+import SettingTable from './SettingTable';
+
+const PersonalSetting = () => {
+    // const [showSetting, setShowSetting] = useState(false);
+    const [show, setShow] = useState(false);
+
+    const showSettingList = () => {
+        setShow(!show);
+     
+      };
+return(
+    <>
+    <div  className="setting-cards">
+    <div
+        className="setting-title-container"
+        onClick={() => showSettingList()}
+      >
+        Personal Setting
+        <MdKeyboardArrowDown size={20} />
+
+      </div>
+      </div>
+    
+    </>
+)
+
+}
+export default PersonalSetting
