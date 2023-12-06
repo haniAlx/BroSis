231624de02c1f76252d8a0f1640e733310eeeac4
Hana447 <alemayehuhana22@gmail.com>
2023-12-06 10:24:58 +0300

    report  update

--- src/pages/Report/CargoReport.js
+++ src/pages/Report/CargoReport.js
@@ -1,205 +1,90 @@
-import { useState, useEffect } from "react";
-import DataTable from "react-data-table-component";
-import { saveAs } from "file-saver";
-import * as XLSX from "xlsx";
 import "./Table.css";
 import * as React from "react";
-import { mainAPI } from "../../components/mainAPI";
-const CargoReport = () => {
-  const jwt = JSON.parse(localStorage.getItem("jwt"));
+import jsPDF from "jspdf";
+import "jspdf-autotable";
+import * as XLSX from "xlsx";
+import { saveAs } from "file-saver";
 
-  const customStyles = {
-    headRow: {
-      style: {
-        backgroundColor: "#034694",
-        color: "white",
-      },
-    },
-    headCells: {
-      style: {
-        fontSize: "14px",
-        fontWeight: "500",
-      },
-    },
-    cells: {
-      style: {
-        fontSize: "15px",
-      },
-    },
-    table: {
-      width: "500px",
-      height: "500px",
-      border: "60px solid #300AC",
-    },
-    rows: {
-      style: {
-        minHeight: "44px",
-      },
-    },
+const CargoReport = ({ currentPageCargo, inputs }) => {
+  const exportPDF = () => {
+    const doc = new jsPDF();
+    doc.autoTable({
+      head: [["Id", "Name", "Business", "Revenue"]],
+      body: inputs.map((item) => [
+        item.id,
+        item.ownerName,
+        item.businessINF.businessName,
+        item.revenue,
+      ]),
+    });
+    doc.save("CargoReport.pdf");
   };
-  const column = [
-    {
-      name: "Index",
-      selector: (row) => row.id,
-      field: "id",
-    },
-    {
-      name: "Name",
-      selector: (row) => row.ownerName,
-      field: "ownerName",
-    },
-    {
-      name: "TotalShipping",
-    },
+  const exportToExcel = () => {
+    const data = inputs.map((item) => ({
+      Id: item.id,
+      Name: item.ownerName,
+      Business: item.businessINF.businessName,
+      Revenue: item.revenue,
+    }));
 
-    {
-      name: "TotalRevenue",
-      selector: (row) => row.revenue,
-      field: "revenue",
-    },
-  ];
+    const ws = XLSX.utils.json_to_sheet(data);
+    const wb = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, "CargoReport");
+    const excelBuffer = XLSX.write(wb, { bookType: "xlsx", type: "array" });
+    saveAsExcelFile(excelBuffer, "CargoReport");
+  };
 
-  const [records, setRecords] = useState([]);
-  const totalRevenue = records.reduce(
-    (total, record) => total + record.revenue,
+  const saveAsExcelFile = (buffer, fileName) => {
+    const data = new Blob([buffer], { type: "application/octet-stream" });
+    saveAs(data, fileName + ".xlsx");
+  };
+
+  const totalRevenue = inputs.reduce(
+    (total, input) => total + input.revenue,
     0
   );
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const headers = {
-          "Content-Type": "application/json",
-          Accept: "application/json",
-          Authorization: `Bearer ${jwt}`,
-        };
-
-        const ownerRes = await fetch(`${mainAPI}/Api/Admin/All/CargoOwners`, { headers });
-
-        if (ownerRes.status === 401) {
-          console.log("Unable to Load!! server responded with 401");
-        }
-        const ownerData = await ownerRes.json();
-        if (ownerData && ownerRes.ok) {
-          console.log(ownerData);
-          setRecords(ownerData.cargoOwners);
-          setFilterRecords(ownerData.cargoOwners);
-        }
-        if (ownerRes.status === 400) {
-          console.log("Invalid API server 400");
-        }
-      } catch (e) {
-        console.log(e);
-      }
-    };
-
-    fetchData();
-  }, []);
-
-  const [filterRecords, setFilterRecords] = useState([]);
-
-  const handleFilter = (event) => {
-    const newData = filterRecords.filter((row) =>
-      row.name.toLowerCase().includes(event.target.value.toLowerCase())
-    );
-    setRecords(newData);
-  };
-  const exportColumns = column
-    .filter((col) => col.name !== "Detail")
-    .map((col) => ({
-      title: col.name,
-      dataKey: col.field,
-    }));
-  const exportPDF = () => {
-    const filteredData = records.map((row) =>
-      exportColumns.reduce((obj, column) => {
-        if (column.dataKey === "city") {
-          obj[column.dataKey] = row.address ? row.address.city : "";
-        } else {
-          obj[column.dataKey] = row[column.dataKey];
-        }
-        return obj;
-      }, {})
-    );
-    import("jspdf").then((jsPDF) => {
-      import("jspdf-autotable").then(() => {
-        const doc = new jsPDF.default(0, 0);
-        doc.autoTable(exportColumns, filteredData);
-        doc.save("CargoReport.pdf");
-      });
-    });
-  };
-  const handleExcelDownload = () => {
-    const workbook = XLSX.utils.book_new();
-    const filteredData = records.map((row) =>
-      exportColumns.reduce((obj, column) => {
-        if (column.dataKey === "city") {
-          obj[column.dataKey] = row.address ? row.address.city : "";
-        } else {
-          obj[column.dataKey] = row[column.dataKey];
-        }
-        return obj;
-      }, {})
-    );
-
-    const worksheet = XLSX.utils.json_to_sheet(filteredData);
-    XLSX.utils.book_append_sheet(workbook, worksheet, "Table Data");
-    const excelData = XLSX.write(workbook, { bookType: "xlsx", type: "array" });
-    const excelBlob = new Blob([excelData], {
-      type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-    });
-    saveAs(excelBlob, "CargoReport.xlsx");
-  };
   return (
-    <div className="tableReport">
-      <div className="table-container"  id='report-Table'>
-
-      <table className="data-table">
+    <div className="tableReports">
+      <div className="table-containers" id="report-Tables">
+        <table className="data-tables">
           <thead>
             <tr>
-              <th>Index</th>
-              <th>Cargo Owner</th>
-              <th>Business Name</th>
+              <th>Id</th>
+              <th>Name</th>
+              <th>Business</th>
               <th>Revenue</th>
             </tr>
           </thead>
 
           <tbody>
-            {records.length > 0 ? (
-              records.map((item, index) => (
-                <tr className="" key={index}>
-                  <td>{index + 1}</td>
-                  <td>{item.ownerName}</td>
-                  <td>{item.businessINF.businessName}</td>
-                  <td>{item.revenue}</td>                 
-                </tr>
-              ))
-            ) : (
-              <tr>
-                <td>NoRecored Found</td>
+            {currentPageCargo.map((item, index) => (
+              <tr className="" key={index}>
+                <td>{index + 1}</td>
+                <td>{item.ownerName}</td>
+                <td>{item.businessINF.businessName}</td>
+                <td>{item.revenue}</td>
               </tr>
-            )}
+            ))}
           </tbody>
         </table>
-
-         
-          <div style={{ display: "flex", justifyContent: "right" }}>
-            Total Revenue: {totalRevenue}
-          </div>
-          <div style={{ gap: "3px", justifyContent: "space-between" }}>
-            <button className="bttn" onClick={exportPDF}>
-              Export PDF
-            </button>
-            <button
-              style={{ marginLeft: "10px" }}
-              className="bttn"
-              onClick={handleExcelDownload}
-            >
-              Export Excel
-            </button>
-          </div>
+        <div style={{ display: "flex", justifyContent: "right" }}>
+          Total Revenue: {totalRevenue}
         </div>
+        <div style={{ gap: "3px", justifyContent: "space-between" }}>
+          <button className="bttn" onClick={exportPDF}>
+            Export PDF
+          </button>
+          <button
+            style={{ marginLeft: "10px" }}
+            className="bttn"
+            onClick={exportToExcel}
+          >
+            Export Excel
+          </button>
         </div>
-  ); 
+      </div>
+    </div>
+  );
 };
 export default CargoReport;
--- src/pages/Report/Report.js
+++ src/pages/Report/Report.js
@@ -1,89 +1,199 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
+import { mainAPI } from "../../components/mainAPI";
+import { Pagination } from "antd";
 import "../home.css";
 import { useLoadContext } from "../../components/context/DataLoadContext";
-import CompanyReport from "./CompanyReport"
-import CompanyChart from './CompanyChart'
-import CargoReport from "./CargoReport"
+import CompanyReport from "./CompanyReport";
+import CompanyChart from "./CompanyChart";
+import CargoReport from "./CargoReport";
 import CargoChart from "./CargoChart";
 import TotalRevenue from "./TotalRevenue";
 
 const Report = () => {
   const { error, setRefresh } = useLoadContext();
-  return (
+  const jwt = JSON.parse(localStorage.getItem("jwt"));
+  const [records, setRecords] = useState([]);
+  const [inputs, setInputs] = useState([]);
 
+  const [page, setPage] = useState(1);
+  const [postPerPage, setpostPerPage] = useState(6);
+  const lastIndexOfPage = page * postPerPage;
+  const firstIndexPage = lastIndexOfPage - postPerPage;
+  const totalPages = records.length;
+  const currentPage = records.slice(firstIndexPage, lastIndexOfPage);
+  const onShowSizeChange = (current, pageSize) => {
+    setpostPerPage(pageSize);
+  };
+
+  const [page2, setPage2] = useState(1);
+  const [postPerPage2, setpostPerPage2] = useState(6);
+  const lastIndexOfPage2 = page2 * postPerPage2;
+  const firstIndexPage2 = lastIndexOfPage2 - postPerPage2;
+  const totalPagesCargo = inputs.length;
+  const currentPageCargo = inputs.slice(firstIndexPage2, lastIndexOfPage2);
+
+  const onShowSizeChangeCargo = (current, pageSize) => {
+    setpostPerPage2(pageSize);
+  };
+  const chartData = {
+    labels: currentPage.map((record) => record.firstName),
+    datasets: [
+      {
+        label: "Vehicle",
+        data: currentPage.map((record) => record.totalVehicles),
+        backgroundColor: "#FF8C00",
+        borderWidth: 1,
+      },
+      {
+        label: "Driver",
+        data: currentPage.map((record) => record.totalDrivers),
+        backgroundColor: "#70b8e9",
+        borderWidth: 1,
+      },
+      {
+        label: "Revenue",
+        data: currentPage.map((record) => record.revenue),
+        backgroundColor: "#f4c430",
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const chartData2 = {
+    labels: currentPageCargo.map((input) => input.ownerName),
+    datasets: [
+      {
+        label: "Revenue",
+        data: currentPageCargo.map((input) => input.revenue),
+        backgroundColor: "#FF8C00",
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  useEffect(() => {
+    const fetchData = async (url, setDataCallback) => {
+      try {
+        const headers = {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+          Authorization: `Bearer ${jwt}`,
+        };
+
+        const response = await fetch(url, { headers });
+
+        if (response.status === 401) {
+          console.log("Unable to Load!! server responded with 401");
+        }
+
+        const data = await response.json();
+
+        if (data && response.ok) {
+          console.log(data);
+          setDataCallback(data.vehicleOwnerINF || data.cargoOwners);
+        }
+
+        if (response.status === 400) {
+          console.log("Invalid API server 400");
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData(`${mainAPI}/Api/Admin/All/VehicleOwners`, setRecords);
+    fetchData(`${mainAPI}/Api/Admin/All/CargoOwners`, setInputs);
+  }, [jwt, mainAPI]);
+  return (
     <div className="main-bar">
       <div className="main-bar-wrapper">
-            <div className='reportWrapper'>
-                    <TotalRevenue />
+        <div className="reportWrapper">
+          <TotalRevenue />
+        </div>
+
+        <div className="reportWrapper">
+          <h2>Total Report / Company </h2>
+          <hr className="hr" />
+          {error ? (
+            <>
+              <p
+                style={{
+                  textAlign: "center",
+                  fontSize: "25px",
+                  marginTop: "50px",
+                  color: "red",
+                }}
+              >
+                {error}
+              </p>
+              <button
+                className="btn w-300 center"
+                onClick={() => setRefresh(true)}
+              >
+                Refresh Page
+              </button>
+            </>
+          ) : (
+            <div className="chart-container">
+              <CompanyReport records={records} currentPage={currentPage} />
+              <CompanyChart chartData={chartData} />
             </div>
-  
-            <div className='reportWrapper' >
-                  <h2>Total Report / Company </h2>
-                  <hr className="hr" />
-                  {error ? (
-                    <>
-                      <p
-                        style={{
-                          textAlign: "center",
-                          fontSize: "25px",
-                          marginTop: "50px",
-                          color: "red",
-                        }}
-                      >
-                        {error}
-                      </p>
-                      <button
-                        className="btn w-300 center"
-                        onClick={() => setRefresh(true)}
-                      >
-                        Refresh Page
-                      </button>
-                    </>
-                  ) : (
-                    <div className="chart-container" >
-                        <CompanyReport />
-                        <CompanyChart />
-                      </div>
-                  )
-                  }
-              </div>
-       {/* *************************Reports PAGE *************** */}
-       
-              <div className='reportWrapper' >
-                <h2>Total Report / Cargo</h2>
-                <hr className="hr" />
-                {error ? (
-                  <>
-                    <p
-                      style={{
-                        textAlign: "center",
-                        fontSize: "25px",
-                        marginTop: "50px",
-                        color: "red",
-                      }}
-                    >
-                      {error}
-                    </p>
-                    <button
-                      className="btn w-300 center"
-                      onClick={() => setRefresh(true)}
-                    >
-                      Refresh Page
-                    </button>
-                  </>
-                ) : (
-                 
-                  <div className="chart-container" >
-                      <CargoReport />
-                  <CargoChart />
-                  </div>
-                )}
-                </div>
+          )}
+          <div className="page">
+          <Pagination
+            onChange={(page) => setPage(page)}
+            pageSize={postPerPage}
+            current={page}
+            total={totalPages}
+            onShowSizeChange={onShowSizeChange}
+          />
+          </div>
+        </div>
+        {/* *************************Reports PAGE *************** */}
+
+        <div className="reportWrapper">
+          <h2>Total Report / Cargo</h2>
+          <hr className="hr" />
+          {error ? (
+            <>
+              <p
+                style={{
+                  textAlign: "center",
+                  fontSize: "25px",
+                  marginTop: "50px",
+                  color: "red",
+                }}
+              >
+                {error}
+              </p>
+              <button
+                className="btn w-300 center"
+                onClick={() => setRefresh(true)}
+              >
+                Refresh Page
+              </button>
+            </>
+          ) : (
+            <div className="chart-container">
+              <CargoReport
+                inputs={inputs}
+                currentPageCargo={currentPageCargo}
+              />
+              <CargoChart chartData2={chartData2} />
+            </div>
+          )}
+          <div className='page'>
+          <Pagination
+            onChange={(page2) => setPage2(page2)}
+            pageSize={postPerPage2}
+            current={page2}
+            total={totalPagesCargo}
+            onShowSizeChange={onShowSizeChangeCargo}
+          />
+          </div>
+        </div>
       </div>
-          
-   </div>
-   
+    </div>
   );
-}
+};
 
 export default Report;
